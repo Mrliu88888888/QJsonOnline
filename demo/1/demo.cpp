f570010bd8f4e180f534cce3982597cd4014f868
Mrliu88888888 <51469101+Mrliu88888888@users.noreply.github.com>
2023-07-19 19:52:25 +0800

    演示代码

---
+++ demo/1/demo.cpp
@@ -0,0 +1,60 @@
+﻿#include "QJsonOnline.h"
+#include <qapplication.h>
+#include <qtextcodec.h>
+#include <qlist.h>
+
+struct SPair
+{
+    SPair() {}
+
+    QString key;
+    QString value;
+
+    QJSON_ONLINE(SPair, key, value);
+};
+
+class Demo
+{
+public:
+    Demo() {}
+
+public:
+    QJSON_ONLINE(Demo, __flag, _class, _0, msg, obj, arr, vec, list);
+
+public:
+    void test()
+    {
+        __flag = true;
+        _class = 8520;
+        qSort(arr.begin(), arr.end());
+        int i = 0;
+        for (auto& l : list) {
+            l.key = QString::number(i);
+            l.value = QString::number(i);
+            ++i;
+        }
+    }
+
+private:
+    bool __flag;
+    int _class;
+    double _0;
+    QString msg;
+    SPair obj;
+    QVector<int> arr;
+    QVector<QString> vec;
+    QList<SPair> list;
+};
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+    QFile f("demo.json");
+    Demo d(f);
+    d.test();
+    d._toJson(f, QJsonDocument::Indented);
+
+    return a.exec();
+}
