5c23232cd6b3eb36c63a2e78ab4bde3588676bd3
Mrliu88888888 <51469101+Mrliu88888888@users.noreply.github.com>
2023-07-20 11:09:50 +0800

    多个文件同时引用QJsonOnline示例

---
+++ demo/2/Person.cpp
@@ -0,0 +1,9 @@
+﻿#include "Person.h"
+#include <qdebug.h>
+
+void Person::show()
+{
+    qDebug() << id;
+    qDebug() << name;
+    qDebug() << age;
+}
---
+++ demo/2/Person.h
@@ -0,0 +1,22 @@
+﻿#ifndef PERSON_H
+#define PERSON_H
+
+#include "QJsonOnline.h"
+
+class Person
+{
+public:
+    Person() {}
+
+    void show();
+
+public:
+    QJSON_ONLINE(Person, id, name, age);
+
+private:
+    QString id;
+    QString name;
+    int age;
+};
+
+#endif // !PERSON_H
