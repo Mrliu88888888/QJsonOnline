ff8cfa731764d78c42637f7909c5a250962226d6
Mrliu88888888 <51469101+Mrliu88888888@users.noreply.github.com>
2023-09-28 13:19:52 +0800

    test code and json

---
+++ test/QJsonOnlineTest.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <QObject>
+
+class QJsonOnlineTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void case_t1();
+    void case_t2();
+    void case_t3();
+    void case_t4();
+    void case_t5();
+    void case_t6();
+    void case_t7();
+    void case_t8();
+    void case_t9();
+    void case_t10();
+    void case_t11();
+    void case_t12();
+};
