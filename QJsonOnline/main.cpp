ef09d84abd175d07060b8763d730a0b7a3762663
Mrliu88888888 <994843566@qq.com>
2023-09-28 16:20:34 +0800

    QJsonOnline

--- QJsonOnline/main.cpp
+++ QJsonOnline/main.cpp
@@ -1,6 +1,6 @@
-#include <QtCore/QCoreApplication>
+﻿#include <QtCore/QCoreApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QCoreApplication a(argc, argv);
 
